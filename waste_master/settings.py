a87216274e4f12419a508da5dc31dc74c8455da5
Ole Andreas Hansen <oleaha@me.com>
2016-02-19 14:49:11 +0100

    API app

--- waste_master/settings.py
+++ waste_master/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'rest_framework',
+    'apps.api',
 )
 
 MIDDLEWARE_CLASSES = (
